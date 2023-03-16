0d127fa3ad691bbb94fb91b07b99d6b6f3d72824
Axmadjonov Abdulloh <abdulloh.7013@gmail.com>
2023-03-16 18:16:25 +0500

    Added IWallet/Wallet Repo/Service

---
+++ Ubee.Service/Interfaces/IWalletService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ubee.Service.Interfaces
+{
+    internal class IWalletService
+    {
+    }
+}
