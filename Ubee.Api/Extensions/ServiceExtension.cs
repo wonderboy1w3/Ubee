6bedb90aecacc82597cb77e9877652c46618cb31
azimochilov <azimochilov226@gmail.com>
2023-05-17 14:32:29 +0300

    fixed appsettings

---
+++ Ubee.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Ubee.Data.IRepositories;
+using Ubee.Data.Repositories;
+
+namespace Ubee.Api.Extensions;
+public static class ServiceExtension
+{
+    public static void AddCustomServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+    }
+}
